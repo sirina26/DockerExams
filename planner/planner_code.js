b2e116a3e0db50a7b701c4dc54ec951c882d92fa
sirine26 <sirinereys18@gmail.com>
2023-11-24 13:16:43 +0100

    EX2

--- planner/planner_code.js
+++ planner/planner_code.js
@@ -12,9 +12,7 @@ const args = () => ({ a: randInt(0, 40), b: randInt(0, 40) })
 const generateTasks = (i) =>
   new Array(i).fill(1).map((_) => ({ type: taskType(), args: args() }))
 
-let workers = [
-  // { url: 'http://localhost:8080', id: '' }
-]
+let workers = ['http://localhost:8080']
 
 const app = express()
 app.use(express.json())
