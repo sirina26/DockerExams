0c1a11a6566ee9ab76bc5bba377fd1b5a14fdc29
sirine26 <sirinereys18@gmail.com>
2023-11-24 15:30:00 +0100

    EX 3 et EX4

--- planner/connect.sh
+++ planner/connect.sh
@@ -1,4 +1,4 @@
 for var in "$@"
 do
-    curl -X POST -H "Content-Type: application/json" -d '{"url": "http://localhost:'"${port}"'", "id": ""}' http://localhost:3000/register
+    curl -X POST localhost:3000/register  -H "Content-Type: application/json"  -d "{\"url\": \"http://localhost:$var\"}"
 done
